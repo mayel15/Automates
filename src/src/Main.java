16163f0bb5edede50e8115a6819aea433b82a552
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-03-03 16:50:42 +0100

    test of the automate

--- src/src/Main.java
+++ src/src/Main.java
@@ -19,12 +19,26 @@ public class Main {
         ArrayList<String> symb = null;
         symb.add("a");
         symb.add("b");
-        Alphabet alphabet = new Alphabet(list);
+        Alphabet alphabet = new Alphabet(symb);
 
         ArrayList<Integer> etats = null;
         etats.add(1); etats.add(2); etats.add(3);
+        Integer etatI = etats.get(0);
+        ArrayList<Integer> etatF = null;
+        etatF.add(etats.get(2));
+
+        ArrayList<Transition> transitions = null;
+        Transition t1 = new Transition(1, "a",1);
+        transitions.add(t1);
+        Transition t2 = new Transition(1, "b",2);
+        transitions.add(t2);
+        Transition t3 = new Transition(2, "b",2);
+        transitions.add(t3);
+        Transition t4 = new Transition(2, "a",3);
+        transitions.add(t4);
+
         Automate automate = new Automate(alphabet, etats, etatI, etatF, transitions);
 
-
+        System.out.println(automate.appartient("a"));
     }
 }
