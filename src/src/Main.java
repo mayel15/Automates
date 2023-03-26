5700eadd36fb9281a31a95cb65fa5ee30531365a
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-03-26 11:05:49 +0200

    add some files of the automates

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,15 +1,12 @@
 package src;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         //System.out.println("Hello world!");
         // read a file
         /*InputStream ins = new FileInputStream("C:\\fichiers\automate1.txt");
@@ -44,11 +41,22 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String[] mots = {"21Ss", "5Svs", "L22L1Ll", "2Ss", "5S5s", "d51l", "2115dvdSvs" };
+        String nomDeFichier = "src/files/testAutomate2.txt";
+        BufferedReader br = new BufferedReader(new FileReader(nomDeFichier));
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (line.equals("# mots")) {
+                while (!(line = br.readLine()).startsWith("#")) {
+                    //alphabetSymboles.add(line);
+                    System.out.println(line + " "  + automate.appartient(line));
+                }
+            }
+        }
+        /*String[] mots = {"21Ss", "5Svs", "L22L1Ll", "2Ss", "5S5s", "d51l", "2115dvdSvs" };
         //String[] mots = {"ab", "abba", "bba", "aba", "abbbba", "aaabbbba"};
         for(String mot : mots){
-            System.out.println(mot + " "+ automate.appartient(mot));
-        }
+            System.out.println(mot + " "  + automate.appartient(mot));
+        }*/
 
         //System.out.println(automate.appartient("ab"));
 
