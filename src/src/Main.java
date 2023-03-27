8be4718e06ca97991078a542ce92d42af166f3f5
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-03-27 09:31:49 +0200

    delete some unnecessary comments

--- src/src/Main.java
+++ src/src/Main.java
@@ -7,34 +7,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //System.out.println("Hello world!");
-        // read a file
-        /*InputStream ins = new FileInputStream("C:\\fichiers\automate1.txt");
-        Scanner obj = new Scanner(ins);
-        while(obj.hasNextLine()) System.out.println(obj.nextLine());*/
-
-        //test
-        /*ArrayList<String> symb = new ArrayList<>();
-        symb.add("a");
-        symb.add("b");
-        Alphabet alphabet = new Alphabet(symb);
-
-        ArrayList<String> etats = new ArrayList<>();
-        etats.add("S1"); etats.add("S2"); etats.add("S3");
-        String etatI = etats.get(0);
-        ArrayList<String> etatF = new ArrayList<>();
-        etatF.add(etats.get(2));
-
-        ArrayList<Transition> transitions = new ArrayList<>();
-        Transition t1 = new Transition("S1", "a","S1");
-        transitions.add(t1);
-        Transition t2 = new Transition("S1", "b","S2");
-        transitions.add(t2);
-        Transition t3 = new Transition("S2", "b","S2");
-        transitions.add(t3);
-        Transition t4 = new Transition("S2", "a","S3");
-        transitions.add(t4);*/
-
         Automate automate = null;
         try {
             automate = new Automate("src/files/automate2.txt");
@@ -46,20 +18,10 @@ public class Main {
         String line;
         while ((line = br.readLine()) != null) {
             if (line.equals("# mots")) {
-                while (!(line = br.readLine()).startsWith("#")) {
-                    //alphabetSymboles.add(line);
+                while (/*(line = br.readLine()) != null &&*/ !(line = br.readLine()).startsWith("#")) {
                     System.out.println(line + " "  + automate.appartient(line));
                 }
             }
         }
-        /*String[] mots = {"21Ss", "5Svs", "L22L1Ll", "2Ss", "5S5s", "d51l", "2115dvdSvs" };
-        //String[] mots = {"ab", "abba", "bba", "aba", "abbbba", "aaabbbba"};
-        for(String mot : mots){
-            System.out.println(mot + " "  + automate.appartient(mot));
-        }*/
-
-        //System.out.println(automate.appartient("ab"));
-
-
     }
 }
